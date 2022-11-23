2945e3735be17a4eb5d63d905c7cb31bae8e7aa1
xyg <xyg@jzzp.com>
2022-11-23 15:39:23 +0800

    优化文件

--- link.ps1
+++
@@ -1,8 +0,0 @@
-
-$path = "./nginx/app/demo/api/wx"
-$link = "./nginx/resty/weixin"
-
-if (Test-Path $path) {
-    Remove-Item -Path $path -Recurse -Force -ErrorAction Stop
-}
-New-Item -Path $path -ItemType Junction -Value $link | Out-Null
